045ed2b8c4169c644030da5ae7502396e7630610
dhkxkvhss <dhkxkvhss@naver.com>
2016-06-18 19:14:17 +0900

    hotfix commit1

--- src/calculator.java
+++ src/calculator.java
@@ -3,7 +3,9 @@ public class calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("report calculator 0620");
+		System.out.println("report calculator");
+		
 	}
 
 }
