ee2492c13e81f02ea6e3739c6116acd5493e29f9
Java2-2014034125 <dhkxkvhs@daum.net>
2016-06-19 19:08:56 +0900

    second commit

--- src/calculator.java
+++ src/calculator.java
@@ -11,7 +11,8 @@ public class calculator extends JFrame implements ActionListener {
 	private JPanel panel;
 	private JButton[] buttons;
 	private JTextField screen;
-	private String[] labels = {"7","8","9","+"," ",
+	private String[] labels = {" "," "," "," ","^2",
+							  "7","8","9","+","%",
 							  "4","5","6","-","AC",
 							  "1","2","3","*","C",
 							  "0",".","+/-","/","="};
@@ -28,11 +29,12 @@ public class calculator extends JFrame implements ActionListener {
 		panel.setLayout(new GridLayout (0,5));
 		screen = new JTextField(30);
 		screen.setText(" ");
-		buttons = new JButton[20];
+		buttons = new JButton[25];
 		
-		for(int i =0; i<20; i++){
+		for(int i =0; i<25; i++){
 			buttons[i] = new JButton(labels[i]);
 			panel.add(buttons[i]);
+			if(i>4)
 			buttons[i].addActionListener(this);
 		}
 		add(screen,BorderLayout.NORTH);
